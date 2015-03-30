cfc2baa480e2b60bc22945b4b09dbe4f7c4182dc
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-30 12:34:00 -0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -3,6 +3,7 @@ function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount >= 100)
     unit = field.unit = "Zillions of unit";
+    field.setAttribut("unit", "zillions of")
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
