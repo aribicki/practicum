c4cde418a7c7ee35fcff4b0e4b9025df12e0ad43
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-30 12:32:37 -0500

    testing/troubleshooting

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ var stock = "1 lemon, 2 cabbages, and 101 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount >= 100)
-    unit = unit.concat(Zillions);
+    unit = field.unit = "Zillions of unit";
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
